749c20e7363e9f4e7cea450e4d286af1f2a271de
Ajay <ajaypratap1705374@gmail.com>
2022-03-01 22:56:25 +0530

    heroku add script

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 const path=require("path");
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const hbs=require('hbs');
 const staticPath=path.join(__dirname,"../public");
 const templatePath=path.join(__dirname,"../templetes/views");
